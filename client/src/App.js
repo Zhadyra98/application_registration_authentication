9aa77386d49e3b513042021981d2253e39b64690
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-07-26 16:20:40 +0400

    feat: login route implement, adding react router dom  and some refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,19 @@
-import {useState} from 'react'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  async function registerUser(event) {
-    event.preventDefault();
-    const response = await fetch('http://localhost:1337/api/register', {
-    method: 'POST', 
-    headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password,
-      }),
-    })
-    const data = await response.json()
-    console.log(data);
-    
-  }
-  return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={registerUser} action="">
-        <input 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          placeholder='Name'
-        />
-        <br />
-        <input 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder='Email'
-        />
-        <br />
-        <input 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder='Password'
-        />
-        <br />
-        <input type="submit" value="Register"/>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/register" element={<Register />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
-export default App;
+export default App
